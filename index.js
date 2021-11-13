c54854dc2624022a61ccc37b6af2d58da3fa4fba
Micheal Raef Magdy <Michael.Magdy@ibm.com>
2021-11-13 19:27:59 +0200

    [x]changed port [x]ignore sqlite file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import routes from "./routes";
 import {ItemService,OrderService} from "./services";
 import db from './data/db';
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 const itemService = new ItemService(db);
 const orderService = new OrderService(db);
 app.use(express.json());
